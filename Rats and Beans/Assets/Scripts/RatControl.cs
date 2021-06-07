87d0aa19857782fda23e8e7e739f879a3255763e
unknown <rudro.sett@gmail.com>
2021-06-06 20:12:53 -0400

    pathfinding stuff!

---
+++ Rats and Beans/Assets/Scripts/RatControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RatControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    [SerializeField] bool check;
+
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        if (check)
+        {
+            //Pathfinding();
+            agent.SetDestination(target.position);
+        }
+    }
+  
+
+}
