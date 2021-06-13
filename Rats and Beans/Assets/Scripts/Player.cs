ca8d9ecc496dd437c662bbdfaf9d8299654354b2
unknown <rudro.sett@gmail.com>
2021-06-13 12:30:48 -0400

    almost fixed collisions

--- Rats and Beans/Assets/Scripts/Player.cs
+++ Rats and Beans/Assets/Scripts/Player.cs
@@ -17,8 +17,7 @@ public class Player : MonoBehaviour {
 
     [SerializeField] public Animator swordanim;
 
-    private bool istouchingrat = false;
-    private Collision2D hit;
+    private Collider2D hit;
 
     void Update() {
         attackCooldown = Mathf.Max(0, attackCooldown - 1 * Time.deltaTime);
@@ -41,8 +40,8 @@ public class Player : MonoBehaviour {
 
         swordanim.Play("Base Layer.swordanimation",0);
   
-        swordanim.Play("Base Layer.New State", 0);
-        if (hitfield.IsTouching(tempcollider2)) {
+        //swordanim.Play("Base Layer.swordidle", 0);
+        if (hitfield.IsTouching(hit)) {
             enemy.GetHit(damage);
         }
         attackCooldown = cooldownAmount;
@@ -52,26 +51,11 @@ public class Player : MonoBehaviour {
         }*/
     }
 
-    //ignore this for now; I was trying to make the attack thingy work against multiple rats
-    /*void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.gameObject.tag == "Rat")
-        {
-            //collision.gameObject.GetComponentInParent<Rat>().GetHit(damage);
-            istouchingrat = true;
-            hit = collision;
-        }
+        Debug.Log("hit!");
+        hit = other;
+    
     }
 
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Rat")
-        {
-            //collision.gameObject.GetComponentInParent<Rat>().GetHit(damage);
-            istouchingrat = false;
-            hit = null;
-        }
-    }*/
-
-   
 }
